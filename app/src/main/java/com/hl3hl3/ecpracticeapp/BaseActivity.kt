da1936eef5d86f366bb43a27e4256f0c08fc8928
Rebecca Hsieh <rebecca.hsieh77@gmail.com>
2021-08-29 16:40:04 +0800

    新增 首頁 layout

---
+++ app/src/main/java/com/hl3hl3/ecpracticeapp/BaseActivity.kt
@@ -0,0 +1,5 @@
+package com.hl3hl3.ecpracticeapp
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity()
---
+++ app/src/main/java/com/hl3hl3/ecpracticeapp/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.hl3hl3.ecpracticeapp
+
+import android.os.Bundle
+
+class HomeActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+    }
+
+}
